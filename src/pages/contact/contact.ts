594719f83bdbbee1c7d0473c3984e20f0aa59e8f
rayureteixeira@gmail.com <willyane.sousa@academico.ifrn.edu.br>
2018-10-17 14:31:54 -0300

    a

---
+++ src/pages/contact/contact.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController, IonicPage, NavParams } from 'ionic-angular';
+import { MensagemPage } from '../mensagem/mensagem'
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html'
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  enviarMensagem(){
+  	this.navCtrl.push(MensagemPage);
+  }
+
+
+}
